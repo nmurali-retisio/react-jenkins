8f2162f2fff4fe6ceff7b02e82682e5e6aae3136
Nirup Murali <nirupg63@gmail.com>
2023-01-18 21:28:20 +0530

    changed concurrently

--- bootstrap.js
+++ bootstrap.js
@@ -12,11 +12,13 @@ result.then(
     function onSuccess(exitInfo) {
       // This code is necessary to make sure the parent terminates 
       // when the application is closed successfully.
+      console.log("success", exitInfo)
       process.exit(0);
     },
     function onFailure(exitInfo) {
       // This code is necessary to make sure the parent terminates 
       // when the application is closed because of a failure.
+      console.log("failure ", exitInfo)
       process.exit(0);
     }
   );
