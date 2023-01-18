ffab27d4563b9457226f69b76952198383118838
Nirup Murali <nirupg63@gmail.com>
2023-01-18 21:30:32 +0530

    added failing test case

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,6 +2,6 @@ describe('template spec', () => {
   it('Checks if Heading is available', () => {
     cy.visit('http://localhost:3000')
     
-    cy.get("h1").contains("Heading")
+    cy.get("h1").contains("Test")
   })
 })
